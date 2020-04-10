e4668ef1de2c1fe03c0483724ac5798587ad8e2d
Erik <uss593@my.utsa.edu>
2020-04-09 21:17:01 -0500

    initialized frontend and backend frameworks

--- backend/newfixers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewfixersConfig(AppConfig):
-    name = 'newfixers'
--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Volumes/SD_Storage/Applications/CS6315/fixers/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
