6c6c33bb5fe631c49eabd736c5c7af8bf69d7b39
Erik <uss593@my.utsa.edu>
2020-04-22 14:14:28 -0500

    added nicer layout to homepage with buttons

--- frontend/gui/src/components/Homesearch.jsx
+++ frontend/gui/src/components/Homesearch.jsx
@@ -1,34 +1,73 @@
 import React from "react";
-import { Button } from "antd";
+import { Row, Col } from "antd";
+
+import PlumberButton from "../Buttons/1.png";
+import ElectricianButton from "../Buttons/2.png";
+import HousekeeperButton from "../Buttons/3.png";
+import LandscaperButton from "../Buttons/4.png";
+import GeneralButton from "../Buttons/5.png";
+import BuilderButton from "../Buttons/6.png";
 
 const Homesearch = () => {
   return (
     <div>
-      <Button
-        href="/search?skill=Electrician"
-        style={{
-          background: "linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%)",
-        }}
-      >
-        Electrician
-      </Button>
-      <Button
-        href="/search?skill=Plumber"
-        style={{
-          background: "linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%)",
-        }}
-      >
-        Plumber
-      </Button>
-      <Button
-        href="/search?skill=Housekeeper"
-        style={{
-          background: "linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%)",
-        }}
-      >
-        Housekeeper
-      </Button>
-      <Button>City Selector (Coming soon!)</Button>
+      <h1>Which service are you looking for today?</h1>
+      <Row gutter={[10, 20]} justify="space-around">
+        <Col>
+          <a href="/search?skill=Electrician">
+            <img
+              src={ElectricianButton}
+              style={{ maxHeight: "125px" }}
+              alt="Search Electricians"
+            />
+          </a>
+        </Col>
+        <Col>
+          <a href="/search?skill=Plumber">
+            <img
+              src={PlumberButton}
+              style={{ maxHeight: "125px" }}
+              alt="Search Plumbers"
+            />
+          </a>
+        </Col>
+        <Col>
+          <a href="/search?skill=Housekeeper">
+            <img
+              src={HousekeeperButton}
+              style={{ maxHeight: "125px" }}
+              alt="Search Housekeepers"
+            />
+          </a>
+        </Col>
+        <Col>
+          <a href="/search?skill=Landscaper">
+            <img
+              src={LandscaperButton}
+              style={{ maxHeight: "125px" }}
+              alt="Search Landscapers"
+            />
+          </a>
+        </Col>
+        <Col>
+          <a href="/search?skill=General">
+            <img
+              src={GeneralButton}
+              style={{ maxHeight: "125px" }}
+              alt="Search General"
+            />
+          </a>
+        </Col>
+        <Col>
+          <a href="/search?skill=Builder">
+            <img
+              src={BuilderButton}
+              style={{ maxHeight: "125px" }}
+              alt="Search Builders"
+            />
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
--- frontend/gui/src/containers/ContractorList.jsx
+++ frontend/gui/src/containers/ContractorList.jsx
@@ -1,24 +1,56 @@
 import React, { Component } from "react";
 import axios from "axios";
-//import queryString from "query-string";
+import { connect } from "react-redux";
 
 import Contractors from "../components/Contractors";
 
 class ContractorList extends Component {
   state = {
+    user: [],
     contractors: [],
   };
 
   componentDidMount() {
     const query = this.props.location.search;
-
     axios.get(`http://127.0.0.1:8000/contractors${query}`).then((res) => {
       this.setState({ contractors: res.data });
     });
   }
+
+  componentDidUpdate() {
+    if (!this.state.user.city) {
+      if (this.props.token) {
+        const options = {
+          headers: {
+            Authorization: `Token ${this.props.token}`,
+          },
+        };
+        axios
+          .get(`http://127.0.0.1:8000/rest-auth/user/`, options)
+          .then((res) => {
+            this.setState({ user: res.data });
+            var city = this.state.user.city;
+            const query = this.props.location.search + "&city=" + city;
+            axios
+              .get(`http://127.0.0.1:8000/contractors${query}`)
+              .then((res) => {
+                this.setState({ contractors: res.data });
+              });
+          });
+      }
+    }
+  }
+
   render() {
-    return <Contractors data={this.state.contractors} />;
+    return <Contractors data={this.state.contractors} {...this.props} />;
   }
 }
 
-export default ContractorList;
+const mapStateToProps = (state) => {
+  return {
+    token: state.token,
+    error: state.error,
+  };
+};
+
+export default connect(mapStateToProps)(ContractorList);
